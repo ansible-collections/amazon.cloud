f49ff79e96ebcc7d5eafe6426e7230f4b0182c4d
GomathiselviS <gomathiselvi@gmail.com>
2023-01-17 12:34:55 -0500

    Moved gouttelette/data content to the collection
    Signed-off-by: GomathiselviS <gomathiselvi@gmail.com>

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -1,3 +1,7 @@
+# Copyright: (c) 2022, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
 import re
 import copy
 import json
--- tests/unit/module_utils/test_core.py
+++ tests/unit/module_utils/test_core.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
-
 import json
 from unittest.mock import MagicMock, Mock
 
