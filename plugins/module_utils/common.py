c3664ddc1d5d9182efbd3eec1ba1190938d4296a
abikouo <abikouo@redhat.com>
2023-06-21 15:26:39 +0200

    Remove work around for user agent as amazon.aws 6.0.0 has been released

--- plugins/module_utils/common.py
+++ plugins/module_utils/common.py
@@ -5,20 +5,3 @@
 
 AMAZON_CLOUD_COLLECTION_NAME = "amazon.cloud"
 AMAZON_CLOUD_COLLECTION_VERSION = "0.3.0"
-
-
-_collection_info_context = {
-    "name": AMAZON_CLOUD_COLLECTION_NAME,
-    "version": AMAZON_CLOUD_COLLECTION_VERSION,
-}
-
-
-def set_collection_info(collection_name=None, collection_version=None):
-    if collection_name:
-        _collection_info_context["name"] = collection_name
-    if collection_version:
-        _collection_info_context["version"] = collection_version
-
-
-def get_collection_info():
-    return _collection_info_context
